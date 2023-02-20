6926fd3c78833d95c06fc4927fe10d9b0c723e2e
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-02-20 09:09:06 +0300

    Проверка переполнения

---
+++ Chapter_03/CheckingForOverflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        Console.WriteLine($"Initinal value: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+    }
+}
+catch (OverflowException)
+{
+    Console.WriteLine("The code overflowed but I caught the exception");
+}
+
+unchecked
+{
+    int y = int.MaxValue + 1;
+    Console.WriteLine($"Initial value: {y}");
+    y--;
+    Console.WriteLine($"After decrementing: {y}");
+    y--;
+    Console.WriteLine($"After decrementing: {y}");
+}    
+    
+    
