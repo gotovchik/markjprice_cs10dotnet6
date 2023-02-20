ffa6a34a9cdd195688c41e8f7a310ceddb38cf1a
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-02-20 09:59:07 +0300

    Проверочная работа по главе 3

---
+++ Chapter_03/Exercises/Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+Console.Write("Введите число от 0 до 255: ");
+string? firstInput = Console.ReadLine();
+Console.Write("Введите второе число от 0 до 255: ");
+string? secondInput = Console.ReadLine();
+try
+{
+    byte a = byte.Parse(firstInput);
+    byte b = byte.Parse(secondInput);
+    int result = (a / b);
+    Console.WriteLine($"{a} divided by {b} is {result}");
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
+}
+
+    
