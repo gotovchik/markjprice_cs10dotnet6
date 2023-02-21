bb77713dc07d0202779e54b4fc599e519d491a64
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-02-21 13:16:04 +0300

    Отладка

---
+++ Chapter_04/Debugging/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+double Add(double x, double y)
+{
+    return x * y;
+}
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+Console.WriteLine($"{a} + {b} = {answer}");
+
+Console.WriteLine("Нажмите любую кнопку для выхода из приложения");
+Console.ReadKey();
