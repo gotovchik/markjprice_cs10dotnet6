c57c8dd5f6e4955e91e7d36727ae12de545425ea
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-03-03 13:19:17 +0300

    Генерация и перехват исключений в функциях

---
+++ Chapter_04/CallStackExceptionHandlingLib/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+namespace Packt;
+public class Calculator
+{
+    public static void Gamma() // public – для вызова вне фукнции
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+    private static void Delta() // private – для вызова только внутри функции
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
