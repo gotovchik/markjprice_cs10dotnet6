17e9e8b9cafdb6ce55150dff01344b61d03d371b
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-03-08 09:49:15 +0300

    Свойства

--- Chapter_05/PacktLibrary/PacktLibrary/PersonAutoGen.cs
+++ Chapter_05/PacktLibrary/PacktLibrary/PersonAutoGen.cs
@@ -14,7 +14,33 @@ namespace Packt.Shared
         }
 
         public string Greeting => $"{Name} says 'Hello!'";
-        public public int Age => DateTime.Today.Year - DateOfBirth.Year;
+        public int Age => DateTime.Today.Year - DateOfBirth.Year;
+
+        public string FavoriteIceCream { get; set; }
+
+        private string favoritePrimaryColor;
+
+        public string FavoritePrimatyColor
+        {
+            get
+            {
+                return favoritePrimaryColor;
+            }
+            set
+            {
+                switch (value.ToLower()) {
+                    case "red":
+                    case "green":
+                    case "blue":
+                        favoritePrimaryColor = value;
+                        break;
+                    default:
+                        throw new System.ArgumentException(
+                            $"{value} is not a primaty color. " +
+                            "Choose from: red, green, blue");
+                }
+            }
+        }
     }
 }
 
