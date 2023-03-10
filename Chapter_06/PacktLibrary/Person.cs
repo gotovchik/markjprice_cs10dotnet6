c3de640d24bbbfc42e9ffbbf75ab0940b86f7da7
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-03-10 06:40:08 +0300

    Дополнительные сведения о методах

---
+++ Chapter_06/PacktLibrary/Person.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person : object
+{
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    // static "reproduce" method
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+
+        p1.Children.Add( baby );
+        p2.Children.Add( baby );
+        return baby;
+    }
+
+    //sample "reproduce" method
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
+    // operator "reproduce"
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Person.Procreate(p1, p2);
+    }
+
+    // method with locale function
+    public static int Factorial(int number)
+    {
+        if (number < 0 )
+        {
+            throw new ArgumentException(
+                $"{nameof(number)} cannot be less than zero.");
+        }
+        return localFactorial(number);
+
+        int localFactorial(int localNumber) // local func
+        {
+            if (localNumber < 1) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+}
---
+++ Chapter_06/PeopleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+// call sample method
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+// call static method
+Person baby2 = Person.Procreate(harry, jill);
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+WriteLine(
+    format: "{0}'s first child is named \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children[0].Name);
+
+// call operator
+Person baby3 = harry * mary;
+
+// call method with local func
+WriteLine($"5! is {Person.Factorial(5)}");
+
+ReadKey();
